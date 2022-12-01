a04399c0ca570fa14df36b723381ba6d30ba106f
LaSta <LavruhinaEA@yandex.ru>
2022-12-01 21:47:47 +0300

    Initial commit

---
+++ new5/src/main/java/Less8/listeners/ButtonListener.java
@@ -0,0 +1,22 @@
+package Less8.listeners;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonListener implements ActionListener {
+
+    private JTextField inputFild;
+
+    public ButtonListener(JTextField inputFild) {
+        this.inputFild = inputFild;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton btn = (JButton) e.getSource();
+        inputFild.setText(inputFild.getText() + btn.getText());
+
+    }
+}
---
+++ new5/src/main/java/Less8/listeners/ClearFieldButtonListener.java
@@ -0,0 +1,21 @@
+package Less8.listeners;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClearFieldButtonListener implements ActionListener {
+    private JTextField inputFild;
+
+    public ClearFieldButtonListener(JTextField inputFild) {
+        this.inputFild = inputFild;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        inputFild.setText("");
+
+    }
+}
